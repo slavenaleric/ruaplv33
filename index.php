f33b3315420a86c7a0a21d37717b35cd82aac3d6
Slaven Alerić <62650991+slavenaleric@users.noreply.github.com>
2024-12-02 17:42:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -67,8 +67,7 @@ register.</p>
     <?php 
  
 // DB connection info 
-// TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved 
-earlier from the Azure Portal. $host = "value of Data Source"; 
+// TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved  
 $host = "slavenaleric-server.mysql.database.azure.com"; 
 $user = "gvvlvivwcq"; 
 $pwd = "3OFFT6TB8ASy$0DU"; 
