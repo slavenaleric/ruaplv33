e3a1218b3d2d7d0e6923a564d55573a68576140a
Slaven Alerić <62650991+slavenaleric@users.noreply.github.com>
2024-12-02 18:02:08 +0100

    update nezz

--- index.php
+++ index.php
@@ -83,11 +83,43 @@ if (mysqli_connect_errno())
  } 
   else 
  { 
- 
+
+  
  // echo '<h3>Connected successfully</h3>'; 
  
  } 
+ $conn = mysqli_connect($host,$user,$pwd); 
  
+ 
+    // Check connection 
+    if (mysqli_connect_errno()) 
+      { 
+      echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
+      } 
+      else { 
+        echo 'Connected successfully'; 
+      } 
+ 
+    $sql = 'CREATE Database ruapdb'; 
+    if ($conn->query($sql) === TRUE) { 
+        echo "Database created successfully"; 
+    } else { 
+        echo "Error creating database: " . $conn->error; 
+    } 
+ 
+    mysqli_select_db($conn,$db); 
+ 
+    $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL 
+    AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email 
+    VARCHAR(30), date DATE);"; 
+ 
+    if ($conn->query($sql_create) === TRUE) { 
+        echo "Table registration_tbl created successfully"; 
+    } else { 
+        echo "Error creating table: " . $conn->error; 
+    } 
+ 
+  $conn->close(); 
 // Insert registration info 
  
 if (!empty($_POST)) 
